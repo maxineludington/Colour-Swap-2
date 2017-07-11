6254fa9e8a62dc028b4987afa84a51f4f1deb005
wFinleyRobinson <w_robinson@dublinschool.org>
2017-07-11 14:37:34 -0500

    Added actions for each button

--- Colour Swap/AlhpabetView.swift
+++ Colour Swap/AlhpabetView.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class AlhpabetView: UIViewController {
-
+    var letterChosen = ""
+    var exampleSentence = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +35,89 @@ class AlhpabetView: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func onButtonPressedA(_ sender: Any) {
+        letterChosen = "a"
+    }
+    
+    @IBAction func onButtonPressedB(_ sender: Any) {
+        letterChosen = "b"
+    }
+    @IBAction func onButtonPressedC(_ sender: Any) {
+        letterChosen = "c"
+    }
+    @IBAction func onButtonPressedD(_ sender: Any) {
+        letterChosen = "d"
+    }
+    @IBAction func onButtonPressedE(_ sender: Any) {
+        letterChosen = "e"
+    }
+    @IBAction func onButtonPressedF(_ sender: Any) {
+        letterChosen = "f"
+    }
+    @IBAction func onButtonPressedG(_ sender: Any) {
+        letterChosen = "g"
+    }
+    @IBAction func onButtonPressedH(_ sender: Any) {
+        letterChosen = "h"
+    }
+    @IBAction func onButtonPressedI(_ sender: Any) {
+        letterChosen = "i"
+    }
+    @IBAction func onButtonPressedJ(_ sender: Any) {
+        letterChosen = "j"
+    }
+    @IBAction func onButtonPressedK(_ sender: Any) {
+        letterChosen = "k"
+    }
+    @IBAction func onButtonPressedL(_ sender: Any) {
+        letterChosen = "l"
+    }
+    @IBAction func onButtonPressedM(_ sender: Any) {
+        letterChosen = "m"
+    }
+    @IBAction func onButtonPressedN(_ sender: Any) {
+        letterChosen = "n"
+    }
+    @IBAction func onButtonPressedO(_ sender: Any) {
+        letterChosen = "o"
+    }
+    @IBAction func onButtonPressedP(_ sender: Any) {
+        letterChosen = "p"
+    }
+    @IBAction func onButtonPressedQ(_ sender: Any) {
+        letterChosen = "q"
+    }
+    @IBAction func onButtonPressedR(_ sender: Any) {
+        letterChosen = "r"
+    }
+    @IBAction func onButtonPressedS(_ sender: Any) {
+        letterChosen = "s"
+    }
+    @IBAction func onButtonPressedT(_ sender: Any) {
+        letterChosen = "t"
+    }
+    @IBAction func onButtonPressedU(_ sender: Any) {
+        letterChosen = "u"
+    }
+    @IBAction func onButtonPressedV(_ sender: Any) {
+        letterChosen = "v"
+    }
+    @IBAction func onButtonPressedW(_ sender: Any) {
+        letterChosen = "w"
+    }
+    @IBAction func onButtonPressedX(_ sender: Any) {
+        letterChosen = "x"
+    }
+    @IBAction func onButtonPressedY(_ sender: Any) {
+        letterChosen = "y"
+    }
+    @IBAction func onButtonPressedZ(_ sender: Any) {
+        letterChosen = "z"
+    }
+    
+    
+    
+    
 
 }
