8e4c4dbdf1a3562a5fcdfcdb8c8c6891ec2dca94
maxineludington <maxine.ludington@gmail.com>
2017-07-12 08:41:22 -0500

    Made it pretty and fixed class names

--- Colour Swap/AlphabetView.swift
+++ Colour Swap/AlphabetView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AlhpabetView: UIViewController {
+class AlphabetView: UIViewController {
     var letterChosenCaps = ""
     var letterChosen = ""
     var exampleSentence = ""
@@ -17,156 +17,156 @@ class AlhpabetView: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     @IBAction func onButtonPressedA(_ sender: Any) {
         letterChosen = "a"
         letterChosenCaps = "A"
-        exampleSentence = "A for Apple"
+        exampleSentence = "A is for Apple!"
     }
     
     @IBAction func onButtonPressedB(_ sender: Any) {
         letterChosen = "b"
         letterChosenCaps = "B"
-        exampleSentence = "B for Baseball"
+        exampleSentence = "B is for Baseball!"
     }
     @IBAction func onButtonPressedC(_ sender: Any) {
         letterChosen = "c"
         letterChosenCaps = "C"
-        exampleSentence = "C for Clock"
+        exampleSentence = "C is for Clock!"
     }
     @IBAction func onButtonPressedD(_ sender: Any) {
         letterChosen = "d"
         letterChosenCaps = "D"
-        exampleSentence = "D for Donkey"
+        exampleSentence = "D is for Donkey!"
     }
     @IBAction func onButtonPressedE(_ sender: Any) {
         letterChosen = "e"
         letterChosenCaps = "E"
-        exampleSentence = "E for Elephant"
+        exampleSentence = "E is for Elephant!"
     }
     @IBAction func onButtonPressedF(_ sender: Any) {
         letterChosen = "f"
         letterChosenCaps = "F"
-        exampleSentence = "F for Father"
+        exampleSentence = "F is for Father!"
     }
     @IBAction func onButtonPressedG(_ sender: Any) {
         letterChosen = "g"
         letterChosenCaps = "G"
-        exampleSentence = "G for Grandmother"
+        exampleSentence = "G is for Grandmother!"
     }
     @IBAction func onButtonPressedH(_ sender: Any) {
         letterChosen = "h"
         letterChosenCaps = "H"
-        exampleSentence = "H for Hungry"
+        exampleSentence = "H is for Hungry!"
     }
     @IBAction func onButtonPressedI(_ sender: Any) {
         letterChosen = "i"
         letterChosenCaps = "I"
-        exampleSentence = "I for Internet"
+        exampleSentence = "I is for Internet!"
     }
     @IBAction func onButtonPressedJ(_ sender: Any) {
         letterChosen = "j"
         letterChosenCaps = "J"
-        exampleSentence = "J for Justice"
+        exampleSentence = "J is for Justice!"
     }
     @IBAction func onButtonPressedK(_ sender: Any) {
         letterChosen = "k"
         letterChosenCaps = "K"
-        exampleSentence = "K for Kangaroo"
+        exampleSentence = "K is for Kangaroo!"
     }
     @IBAction func onButtonPressedL(_ sender: Any) {
         letterChosen = "l"
         letterChosenCaps = "L"
-        exampleSentence = "L for London"
+        exampleSentence = "L is for London!"
     }
     @IBAction func onButtonPressedM(_ sender: Any) {
         letterChosen = "m"
         letterChosenCaps = "M"
-        exampleSentence = "M for Money"
+        exampleSentence = "M is for Money!"
     }
     @IBAction func onButtonPressedN(_ sender: Any) {
         letterChosen = "n"
         letterChosenCaps = "N"
-        exampleSentence = "N for Norway"
+        exampleSentence = "N is for Norway!"
     }
     @IBAction func onButtonPressedO(_ sender: Any) {
         letterChosen = "o"
         letterChosenCaps = "O"
-        exampleSentence = "O for Overtime"
+        exampleSentence = "O is for Overtime!"
     }
     @IBAction func onButtonPressedP(_ sender: Any) {
         letterChosen = "p"
         letterChosenCaps = "P"
-        exampleSentence = "P for Pillow"
+        exampleSentence = "P is for Pillow!"
     }
     @IBAction func onButtonPressedQ(_ sender: Any) {
         letterChosen = "q"
         letterChosenCaps = "Q"
-        exampleSentence = "Q for Question"
+        exampleSentence = "Q is for Question!"
     }
     @IBAction func onButtonPressedR(_ sender: Any) {
         letterChosen = "r"
         letterChosenCaps = "R"
-        exampleSentence = "R for Rabbit"
+        exampleSentence = "R is for Rabbit!"
     }
     @IBAction func onButtonPressedS(_ sender: Any) {
         letterChosen = "s"
         letterChosenCaps = "S"
-        exampleSentence = "S for Superman"
+        exampleSentence = "S is for Superman!"
     }
     @IBAction func onButtonPressedT(_ sender: Any) {
         letterChosen = "t"
         letterChosenCaps = "T"
-        exampleSentence = "T for Telephone"
+        exampleSentence = "T is for Telephone!"
     }
     @IBAction func onButtonPressedU(_ sender: Any) {
         letterChosen = "u"
         letterChosenCaps = "U"
-        exampleSentence = "U for Underwear"
+        exampleSentence = "U is for Underwear!"
     }
     @IBAction func onButtonPressedV(_ sender: Any) {
         letterChosen = "v"
         letterChosenCaps = "V"
-        exampleSentence = "V for Vaccinate"
+        exampleSentence = "V is for Vaccinate!"
     }
     @IBAction func onButtonPressedW(_ sender: Any) {
         letterChosen = "w"
         letterChosenCaps = "W"
-        exampleSentence = "W for World Wide Web"
+        exampleSentence = "W is for World Wide Web!"
     }
     @IBAction func onButtonPressedX(_ sender: Any) {
         letterChosen = "x"
         letterChosenCaps = "X"
-        exampleSentence = "X for Xylophone"
+        exampleSentence = "X is for Xylophone!"
     }
     @IBAction func onButtonPressedY(_ sender: Any) {
         letterChosen = "y"
         letterChosenCaps = "Y"
-        exampleSentence = "Y for Yogurt"
+        exampleSentence = "Y is for Yogurt!"
     }
     @IBAction func onButtonPressedZ(_ sender: Any) {
         letterChosen = "z"
         letterChosenCaps = "Z"
-        exampleSentence = "Z for Zebra"
+        exampleSentence = "Z is for Zebra!"
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
@@ -175,5 +175,5 @@ class AlhpabetView: UIViewController {
         dvc.exampleSentence = exampleSentence
         dvc.letterChosenCaps = letterChosenCaps
     }
-
+    
 }
