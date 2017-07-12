69ea49eeba14f4f27887eca22142bdfb1ebf976a
maxineludington <maxine.ludington@gmail.com>
2017-07-12 08:43:00 -0500

    Deleted excess comments

--- Colour Swap/informationView.swift
+++ Colour Swap/informationView.swift
@@ -18,8 +18,6 @@ class informationView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
         letterChosenLabel.text = "\(letterChosenCaps)\(letterChosen)"
@@ -28,18 +26,5 @@ class informationView: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
